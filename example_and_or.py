6c613b04541e61f9619849706a11cac93f32244c
jose.saavedra <jose.saavedra>
2023-03-23 15:55:23 -0300

    example and or

---
+++ example_and_or.py
@@ -0,0 +1,24 @@
+import numpy as np
+import nn.perceptron as perceptron
+import sklearn.datasets as datasets
+import metrics.metrics as metrics
+import 
+#dataset
+X = np.array([[0,0],[0,1],[1,0],[1,1]], dtype = np.float64)
+y = np.array([0,0,0,1], dtype = np.float64)
+
+n = X.shape[0]
+ 
+#Perceptron
+model = perceptron.Perceptron()
+model.setLoss('bce')
+model.setLearningRate(1)
+model.setSteps(10)
+coeff = model.fit(X, y)
+
+#Evaluation (accuracy x clase)
+y_pred =model.predict(X)
+acc= metrics.accuracy(np.expand_dims(y, axis = 1) , y_pred)
+print('Acc Test {}'.format(acc))
+#S
+
