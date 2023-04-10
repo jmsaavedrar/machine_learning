114294abce1cd5a50b42dbc6bd0cd9123f5b49a6
jose.saavedra <jose.saavedra>
2023-04-10 12:14:51 -0400

    mlp for mnist

---
+++ example_mlp.py
@@ -0,0 +1,71 @@
+"""
+This is an example using MNIST dataset with our own customized MLP
+"""
+
+import tensorflow as tf
+import numpy as np
+import nn.mlp as mlp
+import metrics.metrics as metrics
+import tensorflow.keras.datasets as datasets
+    
+
+# loading dataset
+(x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
+print ('{} {}'.format(x_train.shape, x_train.dtype))
+print ('{} {}'.format(x_test.shape, x_train.dtype))
+print ('{} {}'.format(y_train.shape, y_train.dtype))
+print ('{} {}'.format(y_test.shape, y_train.dtype))
+
+n_train = x_train.shape[0]
+n_test = x_test.shape[0]
+
+#reshape the images to represent 1D arrays -- feature vectors --
+ 
+x_train = np.reshape(x_train, (n_train, -1))
+x_test = np.reshape(x_test, (n_test, -1))
+
+#converting labels to one-hot encoding
+y_train_one_hot = tf.keras.utils.to_categorical(y_train)#to_one_hot(y_train)
+y_test_one_hot = tf.keras.utils.to_categorical(y_test)
+
+x_train = x_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+
+mu = np.mean(x_train, axis = 0)
+
+# normalize data, just centering
+x_train = (x_train - mu) 
+x_test = (x_test - mu)  
+
+# create the model
+mlp = mlp.MLP([256, 128], 10)
+input_vector = tf.keras.Input(x_train.shape)
+mlp(input_vector)    
+mlp.summary()
+
+# defining optimizer
+opt = tf.keras.optimizers.SGD()
+
+# put all together  
+mlp.compile(
+         optimizer=opt,              
+          loss='categorical_crossentropy', 
+          metrics=['accuracy'])
+
+# training or fitting 
+mlp.fit(x_train, 
+        y_train_one_hot, 
+        batch_size=1000,  
+        epochs = 100,
+        validation_data = (x_test, y_test_one_hot))
+
+# prediction using directly the trained model
+# there is also a function called -- predict -- , you can check it  
+y_pred = mlp(x_test, training = False)
+
+# computing confusion_matrix
+mc = metrics.confusion_matrix(y_test, y_pred, 10)
+
+# print mc
+print(mc)
+
--- nn/mlp.py
+++ nn/mlp.py
@@ -1,3 +1,21 @@
-class MLP :
-    def fit(self):
-    def predict(self):
+import tensorflow as tf
+
+class MLP(tf.keras.Model) :
+    #defining components
+    def __init__(self, layer_size, n_classes):
+        super(MLP, self).__init__()         
+        self.layer_list = []
+        for lsize in layer_size:
+            self.layer_list.append(tf.keras.layers.Dense(lsize))
+        self.classifier = tf.keras.layers.Dense(n_classes)
+        
+    # defining architecture
+    def call(self, inputs):
+        x = inputs
+        for mlp_layer in self.layer_list:
+            x = mlp_layer(x)
+            x = tf.keras.activations.sigmoid(x)
+            
+        x = self.classifier(x)
+        x = tf.keras.activations.softmax(x)
+        return x
