14d12a74d0ec091d0cab9dfc8e9be5b157d4ead0
jsaavedra <jose.saavedra>
2023-04-16 18:02:00 -0400

    convnet

---
+++ convnet/simple.py
@@ -0,0 +1,47 @@
+"""
+A simple cnn used for training mnist.
+"""
+import tensorflow as tf
+
+
+class SimpleModel(tf.keras.Model):
+    def __init__(self, number_of_classes):
+        super(SimpleModel, self).__init__()        
+        self.conv_1 = tf.keras.layers.Conv2D(32, (3,3), padding = 'same',  kernel_initializer = 'he_normal')
+        self.max_pool = tf.keras.layers.MaxPool2D((3,3), strides = 2, padding = 'same')
+        self.relu = tf.keras.layers.ReLU();        
+        self.bn_conv_1 = tf.keras.layers.BatchNormalization()
+        self.conv_2 = tf.keras.layers.Conv2D(64, (3,3), padding = 'same',  kernel_initializer='he_normal')
+        self.bn_conv_2 = tf.keras.layers.BatchNormalization()
+        self.conv_3 = tf.keras.layers.Conv2D(128, (3,3), padding = 'same', kernel_initializer='he_normal')
+        self.bn_conv_3 = tf.keras.layers.BatchNormalization()     
+        self.fc1 = tf.keras.layers.Dense(256, kernel_initializer='he_normal')
+        #self.bn_fc_1 = tf.keras.layers.LayerNormalization()
+        self.bn_fc_1 = tf.keras.layers.BatchNormalization()
+        self.fc2 = tf.keras.layers.Dense(number_of_classes)
+
+    # here, connecting the modules
+    def call(self, inputs):        
+        #first block
+        x = self.conv_1(inputs)    
+        x = self.bn_conv_1(x) 
+        x = self.relu(x) 
+        x = self.max_pool(x)    #14 X 14
+        #second block
+        x = self.conv_2(x)  
+        x = self.bn_conv_2(x) 
+        x = self.relu(x) 
+        x = self.max_pool(x)  #7X7
+        #third block
+        x = self.conv_3(x)  
+        x = self.bn_conv_3(x)
+        x = self.relu(x)  
+        x = self.max_pool(x)  #4X4
+        #last block        
+        x = tf.keras.layers.Flatten()(x) 
+        x = self.fc1(x)  
+        x = self.bn_fc_1(x) 
+        x = self.relu(x) 
+        x = self.fc2(x) 
+        x = tf.keras.activations.softmax(x)
+        return x
---
+++ example_convnet.py
@@ -0,0 +1,77 @@
+"""
+This is an example using MNIST dataset with our own customized MLP
+"""
+
+import tensorflow as tf
+import numpy as np
+import convnet.simple as simple
+import metrics.metrics as metrics
+import tensorflow.keras.datasets as datasets
+    
+
+# loading dataset
+(x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
+print ('{} {}'.format(x_train.shape, x_train.dtype))
+print ('{} {}'.format(x_test.shape, x_train.dtype))
+print ('{} {}'.format(y_train.shape, y_train.dtype))
+print ('{} {}'.format(y_test.shape, y_train.dtype))
+
+n_train = x_train.shape[0]
+n_test = x_test.shape[0]
+
+#reshape the images to represent  BxHxWxC
+x_train = x_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+ 
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)) 
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
+
+#converting labels to one-hot encoding
+y_train_one_hot = tf.keras.utils.to_categorical(y_train)#to_one_hot(y_train)
+y_test_one_hot = tf.keras.utils.to_categorical(y_test)
+
+x_train = x_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+
+
+mu = np.mean(x_train, axis = 0)
+
+# normalize data, just centering
+x_train = (x_train - mu) 
+x_test = (x_test - mu)  
+
+# create the model
+model = simple.SimpleModel(10)
+input_vector = tf.keras.Input(x_train.shape[1:])
+model(input_vector)    
+model.summary()
+
+# defining optimizer
+opt = tf.keras.optimizers.SGD()
+ 
+# put all together  
+model.compile(
+         optimizer=opt,              
+          loss='categorical_crossentropy', 
+          metrics=['accuracy'])
+ 
+# training or fitting 
+model.fit(x_train, 
+        y_train_one_hot, 
+        batch_size=256,  
+        epochs = 20,
+        validation_data = (x_test, y_test_one_hot))
+ 
+# prediction using directly the trained model
+# there is also a function called -- predict -- , you can check it  
+y_pred = model(x_test, training = False)
+ 
+# computing confusion_matrix
+mc = metrics.confusion_matrix(y_test, y_pred, 10)
+ 
+# print mc
+print(mc)
+# mc as percentages
+rmc = mc.astype(np.float32) / np.sum(mc, axis = 1, keepdims = True)
+rmc = (rmc * 100).astype(np.int32) / 100 
+print(rmc)
