cb80461495dcf05f093dddc608e9b1632b28cadf
jsaavedra <jose.saavedra>
2023-05-25 09:44:47 -0400

    eigenfaces

---
+++ eigenfaces.py
@@ -0,0 +1,9 @@
+import skimage.io as io
+import matplotlib.pyplot  as plt
+
+if __name__ == '__main__' :
+    list_of_faces = ''
+    image = io.imread(fname)
+    plt.
+    plt.imshow(image, cmap = 'gray')
+    plt.show()
