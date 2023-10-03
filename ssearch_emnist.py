690375bc1311879ccd89c816acc63f640b6519e6
jose.saavedra <jose.saavedra>
2023-10-03 17:56:27 -0300

    ssearch

--- ssearch_emnist.py
+++ ssearch_emnist.py
@@ -1,6 +1,6 @@
 import ssearch.ssearch as ss
 if __name__ == '__main__' :
-    ssearch = ss.SSearch('emnist_model','embedding')
+    ssearch = ss.SSearch('mnist_model','embedding')
     ssearch.load_catalog('data/test_emnist_images.npy', 'data/test_emnist_labels.npy')
     #ssearch.compute_features_on_catalog()    
     ssearch.ssearch_all()
