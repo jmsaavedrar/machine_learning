85cb9fb593011ad1dc591adc673c55bae03b4ad1
jose.saavedra <jose.saavedra>
2023-10-04 15:22:30 -0300

    update ssearc

--- ssearch_emnist.py
+++ ssearch_emnist.py
@@ -1,7 +1,8 @@
 import ssearch.ssearch as ss
 if __name__ == '__main__' :
     ssearch = ss.SSearch('mnist_model','dense1')
-    ssearch.load_catalog('data/test_emnist_images.npy', 'data/test_emnist_labels.npy')        
+    ssearch.load_catalog('data/test_emnist_images.npy', 'data/test_emnist_labels.npy')
+    ssearch.ssearch_all()        
     ssearch.random_save_example(10)
     #cla = ssearch.getClass(5)    
     
