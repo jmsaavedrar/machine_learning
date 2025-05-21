0a42569a5fa582f1821efbf47386744a876d3ec3
jsaa <js.>
2025-05-21 19:01:33 -0400

    mnist predict

---
+++ mnist/predict.py
@@ -0,0 +1,30 @@
+
+import PIL.Image
+import torch
+import model
+import os
+import re
+import PIL
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist_model = model.mnist_conv()
+model_path = 'model_mnist'
+mnist_model.load_state_dict(torch.load(model_path, weights_only=True))
+mnist_model.eval()
+fimage = '/home/data/MNIST-5000/valid_images/digit_mnist_00001_7.png'
+fimage = '/home/data/MNIST-5000/valid_images/digit_mnist_00002_2.png'
+#fimage = '/home/data/MNIST-5000/valid_images/digit_mnist_00011_0.png'
+#fimage = '/home/data/MNIST-5000/valid_images/digit_mnist_00012_6.png'
+image = torch.Tensor(np.array(PIL.Image.open(fimage), dtype = np.float32)) / 255.0 
+image = image.view([1,1,image.size()[0], image.size()[1]])
+
+print(image.shape)
+with torch.no_grad() :
+    logits = mnist_model(image)
+    probs = torch.nn.Softmax()(logits[0])
+    print(logits)
+    
+    cls = torch.Tensor.argmax(probs)
+    print(probs)
+    print('{} {}'.format(cls, probs[cls]))
--- mnist/train.py
+++ mnist/train.py
@@ -9,7 +9,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 #1: dataset loader (defining dataset)
-datapath = '/hd_data/MNIST-5000'
+#datapath = '/hd_data/MNIST-5000'
+datapath = '/home/data/MNIST-5000'
 tr_dataset = dataloader.MNIST_Dataloader(datapath, datatype = 'train')
 tr_dataset = data.DataLoader(tr_dataset, batch_size = 64, shuffle = True)
 
@@ -18,11 +19,11 @@ val_dataset = data.DataLoader(val_dataset, batch_size = 64, shuffle = False)
 
 #2: defining the model
 mnist_model = model.mnist_conv()
-mnist_model.to(device)
+#mnist_model.to(device)
 # defining loss and optimize
 loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(mnist_model.parameters(), lr=0.001, momentum=0.9)
-#optimizer = torch.optim.AdamW(mnist_model.parameters())
+#optimizer = torch.optim.SGD(mnist_model.parameters(), lr=0.001, momentum=0.9)
+optimizer = torch.optim.AdamW(mnist_model.parameters())
 
 
 mnist_model.fit(tr_dataset, val_dataset, optimizer, loss_fn, 50)
