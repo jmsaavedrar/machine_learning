f88c04efd7e9e2cb68a2c12671b47c437504f192
jsaa <js.>
2025-05-30 15:27:01 -0400

    model updaloaded

--- mnist/predict.py
+++ mnist/predict.py
@@ -7,26 +7,62 @@ import re
 import PIL
 import matplotlib.pyplot as plt
 import numpy as np
+import torchmetrics
 
 mnist_model = model.mnist_conv()
-model_path = 'model_mnist'
-mnist_model.load_state_dict(torch.load(model_path, weights_only=True))
+model_path = 'mnist/model_mnist_full'
+mnist_model.load_state_dict(torch.load(model_path, weights_only=True, map_location=torch.device("cpu")))
+
 mnist_model.eval()
 print(mnist_model)
-datapath = '/hd_data/MNIST-5000'
-fimage = os.path.join(datapath, 'valid_images/digit_mnist_00001_7.png')
-#fimage = os.path.join(datapath, 'valid_images/digit_mnist_00002_2.png')
-#fimage = '/home/data/MNIST-5000/valid_images/digit_mnist_00011_0.png'
-#fimage = '/home/data/MNIST-5000/valid_images/digit_mnist_00012_6.png'
-image = torch.Tensor(np.array(PIL.Image.open(fimage), dtype = np.float32)) / 255.0 
-image = image.view([1,1,image.size()[0], image.size()[1]])
+datapath = '/home/data/MNIST-5000'
+fvalid = os.path.join(datapath, 'valid.txt')
+data = []
+with open(fvalid) as f : 
+    for fline in f :
+        iname, icl = fline.split()        
+        iname = os.path.join(datapath, iname)
+        data.append((iname, int(icl)))
+def predict(fimage) : 
+    image = torch.Tensor(np.array(PIL.Image.open(fimage), dtype = np.float32)) / 255.0 
+    image = image.view([1,1,image.size()[0], image.size()[1]])
+    with torch.no_grad() :
+        logits = mnist_model(image)
+        probs = torch.nn.Softmax(dim = 0)(logits[0]).numpy()
+        #print(logits)    
+        cls = np.argmax(probs)
+        # print(probs)
+        # print('{} {}'.format(cls, probs[cls]))
+    return (cls, probs[cls])
 
-print(image.shape)
-with torch.no_grad() :
-    logits = mnist_model(image)
-    probs = torch.nn.Softmax()(logits[0])
-    print(logits)
+result = []
+for fimage, cls in data :
+    cls_p, prob_p = predict(fimage)
+    result.append([cls, cls_p])
+
+result = np.array(result)
+
+
+acc_total = np.equal(result[:,0], result[:,1]).astype(np.int32).mean()  
+print(acc_total)
+acc_per_class = {}
+for c in range(0,10) :
+    idx = np.where(result[:,0] == c)[0]
+    acc = np.equal(result[idx,0], result[idx,1])    
+    acc = np.astype(acc, np.int32).mean()    
+    acc_per_class[c] = acc
     
-    cls = torch.Tensor.argmax(probs)
-    print(probs)
-    print('{} {}'.format(cls, probs[cls]))
+
+print(acc_per_class)
+
+cm = torchmetrics.ConfusionMatrix(task = 'multiclass', num_classes = 10, normalize = 'true')
+x = cm(torch.Tensor(result[:,1]), torch.Tensor(result[:,0]))
+cm.plot(x, cmap = 'jet')
+
+# import seaborn as sn
+# import pandas as pd
+# sn.heatmap(pd.DataFrame(cm.numpy(), range(10), range(10)), annot=True, annot_kws={"size": 16}) # font size
+
+plt.bar(range(0,10), [item[1] for item in acc_per_class.items()])
+plt.show()
+          
